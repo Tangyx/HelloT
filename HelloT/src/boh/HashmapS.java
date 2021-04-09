38cd9b1f1e690bcf0234405dde4fafdf1b7bbf64
admin <admin@DESKTOP-85HPQC5>
2021-04-09 14:21:28 +0800

    1

---
+++ HelloT/src/boh/HashmapS.java
@@ -0,0 +1,12 @@
+package boh;
+
+import java.util.HashMap;
+
+public class HashmapS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new HashMap<Object,Object>();
+	}
+
+}
